99239c0a0fb0fc6b27f017746b9d55ae96db0fdf
faiz9 <sfaiz096@gmail.com>
2023-04-28 10:31:23 -0700

    added openmp parallel loop

--- dgemv-basic.cpp
+++ dgemv-basic.cpp
@@ -8,13 +8,12 @@ const char* dgemv_desc = "Basic implementation of matrix-vector multiply.";
  */
    void my_dgemv(int n, double* A, double* x, double* y) {
 /* Pseudocode Taken from Dr. Bethel's lecture slides x, y are vectors of length N.
- * a is nxn
- * for i=1:n
+ * a is nxn, for i=1:n
  * for j=1:n
  * y[i]=y[i]+A[i,j]*x[j];
  */
   for (int i = 0; i < n; i++) {
-        y[i] += A[i*n] * x[0];   //row-major 
+      // y[i] += A[i*n] * x[0];   this line has errors row-major
         for (int j = 1; j < n; j++) {
             y[i] += A[i*n + j] * x[j]; //accesses i, j
         }
