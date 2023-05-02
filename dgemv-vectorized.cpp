f956dfa4b77b8e80415a3e411e1f93b85f85ad9b
Syed Faiz <sfaiz@mail.sfsu.edu>
2023-05-01 17:18:44 -0700

    Update dgemv-vectorized.cpp

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -1,32 +1,22 @@
-#include <stdlib.h>
-#include <string.h>
-#include <cblas.h> //include header file
-
 const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
 
-/*
- * This routine performs a dgemv operation
- * Y :=  A * X + Y
- * where A is n-by-n matrix stored in row-major format, and X and Y are n by 1 vectors.
- * On exit, A and X maintain their input values.
- */
 void my_dgemv(int n, double* A, double* x, double* y) {
-   double alpha = 1.0, beta = 1.0;
-    int lda = n, incx = 1, incy = 1;
+  for (int i = 0; i < n; i++) {
+        double temp = 0.0;
 
-    // Clear y vector
-    memset(y, 0, n * sizeof(double));
-
-    // Loop unrolling by 2
-    for (int i = 0; i < n; i += 2) {
-        double sum1 = 0.0, sum2 = 0.0;
-
-        for (int j = 0; j < n; j++) {
-            sum1 += A[i * n + j] * x[j];
-            sum2 += A[(i + 1) * n + j] * x[j];
+        // Vectorized inner loop
+        for (int j = 0; j < n; j += 4) {
+            temp += A[i * n + j] * x[j];
+            temp += A[i * n + j + 1] * x[j + 1];
+            temp += A[i * n + j + 2] * x[j + 2];
+            temp += A[i * n + j + 3] * x[j + 3];
         }
 
-        y[i] += sum1;
-        y[i + 1] += sum2;
+        // Handle remaining elements (if n is not divisible by 4)
+        for (int j = n - (n % 4); j < n; j++) {
+            temp += A[i * n + j] * x[j];
+        }
+
+        y[i] += temp;
     }
 }
