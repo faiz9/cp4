7d7535a2bd004926726e00eda02303d0d4e9803b
Syed Faiz <sfaiz@mail.sfsu.edu>
2023-05-01 21:34:01 -0700

    Update dgemv-vectorized.cpp

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -1,4 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <cblas.h>
+#include <immintrin.h>
+
 const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
+
+
+/* void my_dgemv(int n, double* A, double* x, double* y) {
+    // insert your code here: implementation of vectorized vector-matrix multiply
+    double* tmp = (double*) malloc(n * sizeof(double)); // allocate temporary array
+    memset(tmp, 0, n * sizeof(double)); // initialize temporary array to zero
+    // Outer loop: iterate over rows of A
+    for (int i = 0; i < n; i++) {
+        // Inner loop: iterate over columns of A and x
+        for (int j = 0; j < n; j++) {
+            tmp[i] += A[i*n + j] * x[j]; // accumulate intermediate results into tmp
+        }
+    }
+    // Copy temporary array into y
+    memcpy(y, tmp, n * sizeof(double));
+    free(tmp); // free temporary array
+} */
 
 void my_dgemv(int n, double* A, double* x, double* y) {
   for (int i = 0; i < n; i++) {
