16cbce6702f2ce5030f65a0c69557d2ba422b40f
faiz9 <sfaiz096@gmail.com>
2023-05-08 14:50:12 -0700

    fixed inner loop

--- dgemv-openmp-inner.cpp
+++ dgemv-openmp-inner.cpp
@@ -13,12 +13,12 @@ void my_dgemv(int n, double* A, double* x, double* y) {
       int thread_id = omp_get_thread_num();
       printf("Hello world: thread %d of %d checking in. \n", thread_id, nthreads);
    }
-   
+
    for (int i = 0; i < n; i++) {
         
         y[i] += A[i * n] * x[0]; // First element of y[i]
 
-		#pragma omp parallel for //inner loop
+		#pragma omp parallel for reduction(+: y[i]) //inner loop
         for (int j = 1; j < n; j++) {
            // #pragma omp atomic prevents thread issues 
             y[i] += A[i * n + j] * x[j]; // Remaining elements of y[i]
